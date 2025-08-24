8a5628147fa72590a66aa0c34df03db67926b209
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-08-25 00:02:05 +0530

    added mongodb atlas connection via mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
 
 // configuring dotenv
 dotenv.config({
@@ -9,6 +10,15 @@ dotenv.config({
 // initializing express application
 const PORT = process.env.PORT || 3000;
 
+connectDB()
+    .then(() => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    })
+    .catch((err) => {
+        console.error('Failed to connect to the database:', err);
+        process.exit(1);
+    });
+    
 
 //app listener
 app.listen(PORT, () => {

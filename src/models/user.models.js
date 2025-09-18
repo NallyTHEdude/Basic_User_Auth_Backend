403ea3a4a9d36cbed5216ba645e31a7db9bb177d
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-09-18 17:18:24 +0530

    added password encryption and bcrypt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
     avatar: {
@@ -55,6 +55,14 @@ const userSchema = new Schema({
         type: Date,
     },
 }, { timestamps: true });
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+});
 
 const User = mongoose.model("User", userSchema);
 

b351e2f691f41f3130ca6a5a594bf7dad0c9b293
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-09-20 09:26:42 +0530

    added login functionality

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -69,4 +69,51 @@ const registerUser = asyncHandler(async (req, res) => {
 
 });
 
-export {registerUser}
+const login = asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    if(!email){
+        throw new ApiError(400, "Email is required to login");
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new ApiError(400, "User does not exist");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(400, "Invalid Password");
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
+    if(!loggedInUser){
+        throw new ApiError(500, "User registration failed");
+    } 
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {user:loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged in successfully"
+        )
+    );
+
+});
+
+export {registerUser, login}

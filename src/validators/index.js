c6c9f6aaf0acf0b1d8275f71ec15d45ec36d7cae
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-09-20 09:01:22 +0530

    added validation logic for registration

---
+++ src/validators/index.js
@@ -0,0 +1,39 @@
+import {body} from 'express-validator';
+
+const userRegisterValidator = () => {
+    return [
+        body('email')
+            .trim()
+            .notEmpty()
+            .withMessage('Email is required')
+            .isEmail()
+            .withMessage('Email is not valid'),
+
+        body('username')
+            .trim()
+            .notEmpty()
+            .withMessage('Username is required')
+            .isLowercase()
+            .withMessage('Username must be in lowercase')
+            .isLength({min: 3})
+            .withMessage('Username must be atleast 3 characters'),
+
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('Password is required')
+            .isLength({min: 6})
+            .withMessage('Password must be atleast 8 characters'),
+
+        body('fullName')
+            .optional()
+            .trim()
+            .isLength({min: 3})
+            .withMessage('Full name must be atleast 3 characters')
+    ]
+}
+
+
+export {
+    userRegisterValidator
+}

c94be8885f7ef7e8272ac0952249ab0c22735c9a
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-10-29 10:48:16 +0530

    user-model's constants.js import updated

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
-import { AvailableUserRoles, UserRolesEnum } from './constants.js'; // adjust path as needed
+import { AvailableUserRoles, UserRolesEnum } from '../utils/constants.js'; // adjust path as needed
 
 
 const userSchema = new Schema(
